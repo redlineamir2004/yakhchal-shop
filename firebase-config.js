a4089d87faf8e52d6840511367233897b5a25627
redlineamir2004 <ramir3454@gmail.com>
2026-07-05 09:06:19 +0330

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-// اطلاعات پروژه Firebase شما (yakhchal-shop)
-const firebaseConfig = {
-  apiKey: "AIzaSyCq1nlRsDlVOm0rAayL76inlN9S9hxSKC8",
-  authDomain: "yakhchal-shop.firebaseapp.com",
-  projectId: "yakhchal-shop",
-  storageBucket: "yakhchal-shop.firebasestorage.app",
-  messagingSenderId: "1087517278328",
-  appId: "1:1087517278328:web:8c0fff34b311912f81ed8d"
-};
